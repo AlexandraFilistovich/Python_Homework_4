8c933b8555c23aa391158ed2bc4cbacdb4ea559f
AlexandraFilistovich <40547683+AlexandraFilistovich@users.noreply.github.com>
2019-05-23 12:22:47 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-
-l = 'spam and eggs or eggs with spam'
-
-count = Counter(l)
-
-for i in range(len(count.most_common())):
-    print('Symbol:', count.most_common()[i][0], 'Amount of symbol:', count.most_common()[i][1])
